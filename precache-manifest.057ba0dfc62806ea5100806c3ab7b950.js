c9affe6019132f29b64680b0e4f91aab3918453b
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-10 14:07:49 -0500

    Updates

---
+++ precache-manifest.057ba0dfc62806ea5100806c3ab7b950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a653847db3a402c7af794f9d12a6c07",
+    "url": "/ttp-assignment8/index.html"
+  },
+  {
+    "revision": "9452f7e000451d7287e5",
+    "url": "/ttp-assignment8/static/css/main.e68fa3ff.chunk.css"
+  },
+  {
+    "revision": "8369a7a48c15d1093491",
+    "url": "/ttp-assignment8/static/js/2.0768ef27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ttp-assignment8/static/js/2.0768ef27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9452f7e000451d7287e5",
+    "url": "/ttp-assignment8/static/js/main.59fd9d94.chunk.js"
+  },
+  {
+    "revision": "1fd96f31aa9177e31b18",
+    "url": "/ttp-assignment8/static/js/runtime-main.822173a9.js"
+  }
+]);
