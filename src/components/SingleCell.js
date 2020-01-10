8449d8aa91ef0a99af0a7a3a7aa067060de62711
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-10 14:00:52 -0500

    fixing errors

--- src/components/SingleCell.js
+++ src/components/SingleCell.js
@@ -27,25 +27,31 @@ export default class SingleCell extends React.Component {
         }
     }
 
+    componentDidUpdate(prevProps, prevState) {
+
+        if(prevProps !== this.props) {
+            let myStates = this.props.myStates();
+
+            let isFilled = myStates.fillAll;
+            let isUnfilled = myStates.fillUncolored;
+    
+            if(isFilled) {
+                this.setState({
+                    cellColor: this.props.myStates().color
+                })
+            }
+    
+            if(isUnfilled && this.state.cellColor === "") {
+    
+                this.setState({
+                    cellColor: this.props.myStates().color
+                })
+            }
+        }
+
+    }
+
     render() {
-
-        let myStates = this.props.myStates();
-
-        let isFilled = myStates.fillAll;
-        let isUnfilled = myStates.fillUncolored;
-
-        if(isFilled) {
-            this.setState({
-                cellColor: this.props.myStates().color
-            })
-        }
-
-        if(isUnfilled && this.state.cellColor === "") {
-
-            this.setState({
-                cellColor: this.props.myStates().color
-            })
-        }
 
         return (
             <td
