88e44ec18782272334616c091578be36fcb83492
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-10 10:45:18 -0500

    changed app to class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <select>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-            <option value="red">Red</option>
-            <option value="purple">Purple</option>
-        </select>
-    </div>
-  );
+export default class App extends Component {
+
+    
+    render() {
+        return (
+            <div className="App">
+                <select>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="red">Red</option>
+                    <option value="purple">Purple</option>
+                </select>
+            </div>
+          );
+    }
+
 }
 
-export default App;
